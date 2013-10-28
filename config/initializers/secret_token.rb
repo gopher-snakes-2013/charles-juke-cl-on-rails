5a34e95450a2943a5a718ddfa1a0d710d6120b5c
Dev Bootcamp <Apprentice@dbc35.local>
2013-10-28 14:25:08 -0700

    correcting initial commit with all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigslist::Application.config.secret_token = '5603ab57da5d54ef93a19bb3f1accaaa14b03053c4fdb51f02ffe59d3a1c2cb9e79c60bf1b634ce0c1dafcb23e431c8452ea5715d5006c5b3154851f29433723'
